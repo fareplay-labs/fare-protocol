18eaa9277ae576cddfcbb4e2e9d9bf9d2e5224e2
Zynkah <ffgurl07@yahoo.com>
2026-01-26 17:25:38 -0600

    faq mock data added, yup and formik installed, contact from file created

---
+++ src/data/faqData.tsx
@@ -0,0 +1,26 @@
+export const FaqData = [
+  {
+    id: 1,
+    question: "What is Fare Protocol?",
+    answer:
+      "Fare Protocol is a decentralized platform that allows users to deploy permissionless and trustless on-chain casinos using the FARE token.",
+  },
+  {
+    id: 2,
+    question: "How do I make my own casino?",
+    answer:
+      "To make your own casino, you need to use the Fare Protocol platform and deploy your casino smart contract using the FARE token.",
+  },
+  {
+    id: 3,
+    question: "Where can I buy Fare Token?",
+    answer:
+      "You can buy Fare Token on various cryptocurrency exchanges that list the FARE token.",
+  },
+  {
+    id: 4,
+    question: "How do I join the Fare Fam?",
+    answer:
+      "To join the Fare Fam, you can participate in our community channels, follow us on social media, and engage with other members of the Fare Protocol community.",
+  },
+];
--- src/pages/faq/index.tsx
+++ src/pages/faq/index.tsx
@@ -1,7 +1,25 @@
+import { FaqData } from "../../data/faqData";
+
+interface faqDataCellProps {
+  id: number;
+  question: string;
+  answer: string;
+}
+
+const faqDataCell = ({ id, question, answer }: faqDataCellProps) => {
+  return (
+    <div key={id}>
+      <h2>{question}</h2>
+      <p>{answer}</p>
+    </div>
+  );
+};
+
 export const FaqPage = () => {
   return (
     <div className="page-wrapper">
-      <h1>FAQ Page</h1>
+      <h1>FAQ</h1>
+      {FaqData.map((data) => faqDataCell(data))}
     </div>
   );
 };
