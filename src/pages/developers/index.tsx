9600ac3fbd598cdf2d56ecc076aaec1350699f40
Zynkah <ffgurl07@yahoo.com>
2026-01-28 20:01:07 -0600

    adjusted screen size css

--- src/pages/developers/index.tsx
+++ src/pages/developers/index.tsx
@@ -1,7 +1,14 @@
+
+
 export const DevelopersPage = () => {
   return (
     <div className="page-wrapper">
       <h1>Developers Page</h1>
+      <h2>IPFS</h2>
+      <h2>Custom Domain</h2>
+      <h2>Custom UI</h2>
+      <h2>Custom Backend</h2>
+      <h2>Privy NPM</h2>
     </div>
   );
 };
