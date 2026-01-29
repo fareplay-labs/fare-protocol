8a231412a4e02c2883050d04bb9511d783c33934
Zynkah <ffgurl07@yahoo.com>
2026-01-29 10:37:50 -0600

    section placeholder

--- src/pages/swag/index.tsx
+++ src/pages/swag/index.tsx
@@ -3,6 +3,26 @@ import FarePatch from "../../assets/pngs/fare-patch.png";
 import CasinoSticker from "../../assets/pngs/casino-sticker.png";
 import FareBeanie from "../../assets/pngs/fare-beanie.png";
 
+const featuredItems = [
+  { title: "Fare Patches", imageSrc: FarePatch },
+  { title: "Beanies", imageSrc: FareBeanie },
+  { title: "Stickers", imageSrc: CasinoSticker },
+
+];
+
+const FeaturedCard = ({
+  title,
+  imageSrc,
+}: {
+  title: string;
+  imageSrc: string;
+}) => (
+  <div className="card">
+    <h3>{title}</h3>
+    <img src={imageSrc} alt={title} width={200} />
+  </div>
+);
+
 export const SwagPage = () => {
   return (
     <div className="page-wrapper">
@@ -20,19 +40,15 @@ export const SwagPage = () => {
 
       {/* trending section */}
       <div className="cards-wrapper">
+        <h2>FEATURED ITEMS</h2>
         <div className="cards-container">
-          <div className="card">
-            <h3>Fare Patches</h3>
-            <img src={FarePatch} alt="Fare Patch"  width={200} />
-          </div>
-          <div className="card">
-            <h3>Beanies</h3>
-            <img src={FareBeanie} alt="Fare Beanie"  width={200} />
-          </div>
-          <div className="card">
-            <h3>Stickers</h3>
-            <img src={CasinoSticker} alt="Casino Sticker" width={200} />
-          </div>
+          {featuredItems.map((item, idx) => (
+            <FeaturedCard
+              key={item.title + idx}
+              title={item.title}
+              imageSrc={item.imageSrc}
+            />
+          ))}
         </div>
       </div>
     </div>
