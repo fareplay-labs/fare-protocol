677298a4d52d65d5076f30f5a5b0bb464cca8088
Zynkah <ffgurl07@yahoo.com>
2026-02-28 11:01:59 -0600

    speed insights added

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
       <Analytics />
+      <SpeedInsights />
     </BrowserRouter>
   </StrictMode>,
 );
